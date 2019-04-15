c8265e4af280fe4b6bed49ddaca5c1851039de59
leheller <49327345+leheller@users.noreply.github.com>
2019-04-15 10:32:42 -0400

    Update Register.py

--- Register.py
+++ Register.py
@@ -15,6 +15,8 @@ def init(data):
     data.username = ""
     data.password = ""
     data.confirmPassword = ""
+    data.GPA = ""
+    data.school = ""
 
 def mousePressed(event, data):
     if event.x>=data.width//2-data.sizeX and event.x<=data.width//2+data.sizeX:
@@ -23,17 +25,21 @@ def mousePressed(event, data):
         elif event.y<=data.sizeX+2.5*data.sizeY and event.y>=data.sizeX+1.5*data.sizeY:
             data.boxState = (True,"password")
         elif event.y>=data.sizeX+3*data.sizeY and event.y<=data.sizeX+4*data.sizeY:
-            data.boxState = ("confirmPassword")
+            data.boxState = (True,"confirmPassword")
+        else:
+            data.boxState = (False, "")
     else:
-        data.boxState = (False, 0, 0)
+        data.boxState = (False, "")
         
 def keyPressed(event, data):
     if event.keysym == "Return":
         data.boxState == "login"
-    elif data.boxState[0] == True and data.boxState[3] == "username":
+    elif data.boxState[0] == True and data.boxState[1] == "username":
         data.username += event.char
-    elif data.boxState[0] == True and data.boxState[3] == "password":
+    elif data.boxState[0] == True and data.boxState[1] == "password":
         data.password += event.char
+    elif data.boxState[0] == True and data.boxState[1] == "confirmPassword":
+        data.confirmPassword += event.char
         
 def redrawAll(canvas, data):
     canvas.create_rectangle(0,0,data.width,data.height,fill="green")
@@ -44,6 +50,8 @@ def redrawAll(canvas, data):
     #password box
     canvas.create_rectangle(data.width//2-data.sizeX,data.sizeX+1.5*data.sizeY, data.width//2+data.sizeX,data.sizeX+2.5*data.sizeY,fill="white",activefill="yellow",width=2)
     canvas.create_rectangle(data.width//2-data.sizeX,data.sizeX+3*data.sizeY, data.width//2+data.sizeX,data.sizeX+4*data.sizeY,fill="white",activefill="yellow",width=2)
+    #GPA box
+    canvas.create_rectangle(data.width//3-data.sizeX//2,data.sizeX+5.5*data.sizeY, data.width//3+data.sizeX//2,data.sizeX+6.5*data.sizeY,fill="white",activefill="yellow",width=2)
     #create profile text
     canvas.create_text(data.width//2,data.size,anchor="c",font="ComicSans 28 bold",text="Create Profile",fill="white")
     #username
@@ -51,11 +59,19 @@ def redrawAll(canvas, data):
     #password 
     canvas.create_text(data.width//8,data.sizeX+1.5*data.sizeY,anchor="c",font="ComicSans 18 bold",text="password:",fill="white")
     canvas.create_text(data.width//8,data.sizeX+3*data.sizeY,anchor="c",font="ComicSans 18 bold",text="confirm\npassword:",fill="white")
+    #GPA text
+    canvas.create_text(data.width//8,data.sizeX+5.8*data.sizeY,anchor="c",font="ComicSans 18 bold",text="GPA:",fill="white")
     #username text
-    canvas.create_text(data.width//2,data.sizeX,anchor="c",text=data.username)
+    canvas.create_text(data.width//2,data.sizeX+data.sizeY//2,anchor="c",text=data.username)
     #password text
-    canvas.create_text(data.width//2,data.sizeX*2,anchor="c",text=data.password)
-    canvas.create_text(data.width//2,data.sizeX*3,anchor="c",text=data.confirmPassword)
+    canvas.create_text(data.width//2,data.sizeX+2*data.sizeY,anchor="c",text=data.password)
+    canvas.create_text(data.width//2,data.sizeX+3.5*data.sizeY,anchor="c",text=data.confirmPassword)
+    #GPA text
+    canvas.create_text(data.width//3,data.sizeX+6*data.sizeY,anchor="c",text=data.GPA)
+    #image box
+    canvas.create_rectangle(data.width//2,data.height//2,data.width-data.size,data.height-data.size,fill="black")
+    #image text
+    canvas.create_text(3*data.width/4-data.size//2,3*data.height/4-data.size//2,anchor="c",text="upload image",fill="white",activefill="yellow")
 
 ####################################
 # use the run function as-is
